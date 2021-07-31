3abf4fd2812a44c8757f3679a556162bd6b6be0c
lejaaprianza <lejaprianza@gmail.com>
2021-07-31 22:07:31 +0700

    tutorial 9

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import "./App.css";
-import {
-  FunctionalComponent1,
-  FunctionalComponent2,
-} from "./components/FunctionalComponent";
+
+export const panggilFungsi = () => {
+  console.log("selamat datang");
+  return "tes";
+};
+
+const panggilFungsiParam = (nama) => {
+  console.log("ini ", nama);
+};
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* {panggilFungsi()} */}
+      <button onClick={() => console.log("tes console log")}>
+        tombol console log
+      </button>
+      <button onClick={panggilFungsi}>Panggil fungsi</button>
+      <button
+        onClick={() => {
+          panggilFungsiParam("Budi");
+        }}
+      >
+        Panggil fungsi dengan parameter
+      </button>
+    </div>
+  );
 };
 
 export default App;
